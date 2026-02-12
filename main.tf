31d653eba1de8bf776a3c3918bb147279c58ef79
ravi-ka-hcl <ravi-ka@hcltech.com>
2026-02-12 15:19:33 +0530

    Create main.tf demo -7

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+}
+
+terraform { 
+  cloud { 
+    
+    organization = "myvcpDemo" 
+
+    workspaces { 
+      name = "API-githubTerracloud" 
+    } 
+  } 
+}
+
+
+provider "azurerm" {
+  features {}
+}
+
+
+# Create a resource group
+resource "azurerm_resource_group" "example-7" {
+  name     = "example-resources-ter-Demo-7"
+  location = "East US"
+}
